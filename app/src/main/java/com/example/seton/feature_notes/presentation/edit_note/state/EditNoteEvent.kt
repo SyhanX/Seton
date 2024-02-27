da742a1c75fa30296b33de1015fc103debd59358
Syhan <oboznyy.maks99@gmail.com>
2024-02-27 21:35:52 +0700

    add new method

--- app/src/main/java/com/example/seton/feature_notes/presentation/edit_note/state/EditNoteEvent.kt
+++ app/src/main/java/com/example/seton/feature_notes/presentation/edit_note/state/EditNoteEvent.kt
@@ -1,8 +1,9 @@
-package com.example.seton.feature_notes.presentation.edit_note
+package com.example.seton.feature_notes.presentation.edit_note.state
 
 sealed class EditNoteEvent {
     data class EnterTitle(val title: String): EditNoteEvent()
     data class EnterContent(val content: String): EditNoteEvent()
+    data class InsertImage(val byteArray: ByteArray): EditNoteEvent()
     data object SaveNote: EditNoteEvent()
     data object DeleteNote: EditNoteEvent()
 }
