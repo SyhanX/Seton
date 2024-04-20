e1fd5315e1fe6dceabf07cfc8882494ee9bb53df
Syhan <oboznyy.maks99@gmail.com>
2024-04-20 17:02:16 +0700

    fix bugs

--- app/src/main/java/com/example/seton/feature_notes/presentation/note_list/state/NoteCardState.kt
+++ app/src/main/java/com/example/seton/feature_notes/presentation/note_list/state/NoteCardState.kt
@@ -7,4 +7,26 @@ data class NoteCardState(
     val isChecked: Boolean,
     val onClick: (Int, () -> Unit) -> Unit,
     val onLongClick: (Int) -> Unit
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NoteCardState
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (content != other.content) return false
+        if (isChecked != other.isChecked) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + title.hashCode()
+        result = 31 * result + content.hashCode()
+        result = 31 * result + isChecked.hashCode()
+        return result
+    }
+}
