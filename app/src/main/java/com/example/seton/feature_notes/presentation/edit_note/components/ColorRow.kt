585f31a28b278a83b36883594b55628e7a081d00
SyhanX <oboznyy.maks99@gmail.com>
2024-11-28 18:34:23 +0700

    optimize imports

--- app/src/main/java/com/example/seton/feature_notes/presentation/edit_note/components/ColorRow.kt
+++ app/src/main/java/com/example/seton/feature_notes/presentation/edit_note/components/ColorRow.kt
@@ -1,24 +1,12 @@
 package com.example.seton.feature_notes.presentation.edit_note.components
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import com.example.seton.R
 import com.example.seton.common.presentation.state.ContainerColor
 
 @Composable
