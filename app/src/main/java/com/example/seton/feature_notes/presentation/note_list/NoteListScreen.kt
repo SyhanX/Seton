0e91861b6967d0038dbb0ccc892c451dbac93a7c
SyhanX <oboznyy.maks99@gmail.com>
2024-12-17 04:17:38 +0700

    add flavor text when there's no notes

--- app/src/main/java/com/example/seton/feature_notes/presentation/note_list/NoteListScreen.kt
+++ app/src/main/java/com/example/seton/feature_notes/presentation/note_list/NoteListScreen.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,16 +18,19 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.seton.R
@@ -167,13 +172,25 @@ private fun NoteListContent(
             }
         }
     ) { innerPadding ->
-        DynamicLazyLayout(
-            isGridLayout = isGridLayout.value,
-            innerPadding = innerPadding,
-            items = notes,
-            onLongCardClick = { onLongCardClick(it) },
-            onCardClick = { onCardClick(it) }
-        )
+        if (notes.isEmpty()) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = stringResource(R.string.no_notes),
+                    fontSize = 20.sp
+                )
+            }
+        } else {
+            DynamicLazyLayout(
+                isGridLayout = isGridLayout.value,
+                innerPadding = innerPadding,
+                items = notes,
+                onLongCardClick = { onLongCardClick(it) },
+                onCardClick = { onCardClick(it) }
+            )
+        }
     }
 }
 
